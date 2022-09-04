da4975a439c3e3a497326c426b77051ecfbfc788
hzhoup <hzhouping@outlook.com>
2022-09-04 16:57:09 +0800

    build: 项目初始化

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    svelte({
+      compilerOptions: {
+        customElement: true,
+      },
+    }),
+  ],
+  build: {
+    lib: {
+      entry: './src/main.ts',
+      name: 'skittles-ui',
+    },
+  },
+})
